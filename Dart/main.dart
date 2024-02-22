505ed50c47c5f57ad75e9ae89fe084f74f0b8276
Ondieki Ernest <41272301+OndiekiErnest@users.noreply.github.com>
2024-02-22 20:38:08 +0300

    Initial commit
    First program
    Variables and data types

---
+++ Dart/main.dart
@@ -0,0 +1,14 @@
+void main() {
+  print("Hello guys!");
+  // dart variables
+  String name = "Sam"; // string type
+  var location = "Earth"; // var is general
+  int age = 54; // integer type
+  double height = 2.1; // decimal
+  bool newbie = true;
+  // format for printing
+  var to_print =
+      "I'm $name, $age years old, from $location.\nHeight: $height m\nNewbie: $newbie";
+  // print to_print object
+  print(to_print);
+}
